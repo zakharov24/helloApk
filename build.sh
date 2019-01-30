95903ccbd616a3525de2abda1559d3ade6f988b8
Alexander Zakharov <mail@zakharov.ru.net>
2019-01-30 18:41:55 +0700

    Apk builder

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+SOURCE=agora/voting
+BASE=/usr/lib
+SDK="${BASE}/android-sdk"
+BUILD_TOOLS="${SDK}/build-tools/debian"
+PLATFORM="${SDK}/platforms/android-23"
+mkdir -p build/gen build/obj build/apk
+"${BUILD_TOOLS}/aapt" package -f -m -J build/gen/ -S res -M AndroidManifest.xml -I "${PLATFORM}/android.jar"
+javac -source 1.7 -target 1.7 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
+		 -classpath "${PLATFORM}/android.jar" -d build/obj \
+		 build/gen/${SOURCE}/R.java java/${SOURCE}/*.java
+"${BUILD_TOOLS}/dx" --dex --output=build/apk/classes.dex build/obj/
+"${BUILD_TOOLS}/aapt" package -f -M AndroidManifest.xml -S res/  -I "${PLATFORM}/android.jar" \
+		-F build/Voting.unsigned.apk build/apk/
+"${BUILD_TOOLS}/zipalign" -f 4 build/Voting.unsigned.apk build/Voting.aligned.apk
+apksigner sign --ks keystore.jks \
+        --ks-key-alias androidkey --ks-pass pass:android \
+		      --key-pass pass:android --out build/Voting.apk \
+			  build/Voting.aligned.apk
